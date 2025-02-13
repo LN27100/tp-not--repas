00162354a0d6ba67cec57c82a071db3fd9aceaee
LN27100 <heloceinlovehotmail.fr>
2025-02-13 09:45:56 +0100

    correctif des configurations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,23 @@
-import { ClerkProvider } from '@clerk/clerk-expo';
-import { Slot } from 'expo-router';
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
+import { tokenCache } from "../libs/cache";
 
-export default function Layout() {
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
-  if (!publishableKey) {
-    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY to your .env file');
-  }
-
-  return (
-    <ClerkProvider publishableKey={publishableKey}>
-      <Slot />
-    </ClerkProvider>
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
   );
 }
+
+const RootLayout = () => {
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayout;
